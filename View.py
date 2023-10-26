63bbfde28a67a1db44efbb7bf8e1fd9b35d9d7dd
TH4YSZ <thainacristina.sz07@gmail.com>
2023-10-26 08:43:55 -0300

    Novo menu

--- View.py
+++ View.py
@@ -6,7 +6,7 @@ sair = False
 while sair == False:
     os.system("cls")
     print("SOFTWARE DE TO-DO \n")
-    menu = input("[1] Adicionar tarefa\n[2] Listar tarefas \n[3] Excluir tarefa \n[4] Sair \n \nDigite a opção desejada: ")
+    menu = input("[1] Adicionar tarefa\n[2] Listar tarefas \n[3] Alterar tarefa \n[4] Concluir tarefa \n[5] Listar tarefas concluídas \n[6] Excluir tarefa \n[7] Sair \n \nDigite a opção desejada: ")
     match menu:
         case "1":
             os.system("cls")
@@ -21,15 +21,39 @@ while sair == False:
             print("")
             os.system("pause")
             os.system("cls") 
+        
         case "3":
             os.system("cls")
-            print("-- EXCLUIR TAREFA ---\n")
+            print("--- ALTERAR TAREFA ---\n")
+            alt = input("Digite o ID da tarefa que deseja alterar: ")
+            print("")
+            os.system("pause")
+            os.system("cls") 
+        
+        case "4":
+            os.system("cls")
+            print("--- CONCLUIR TAREFA ---\n")
+            clr = input("Digite o ID da tarefa que deseja concluir: ")
+            print("")
+            os.system("pause")
+            os.system("cls") 
+
+        case "7":
+            os.system("cls")
+            print("--- LISTAR TAREFAS CONCLUÍDAS ---\n")
+            print("")
+            os.system("pause")
+            os.system("cls") 
+
+        case "6":
+            os.system("cls")
+            print("--- EXCLUIR TAREFA ---\n")
             listarTarefa = ControllerListarTarefa()
             excluir = input("\nQual o índice da tarefa que deseja excluir: ")
             excluirTarefa = ControllerExcluirTarefa(excluir)
             os.system("pause")
             os.system("cls")
-        case "4":
+        case "7":
             sair = True
         case _:
             print("\nOpção inválida.")
